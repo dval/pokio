a65ecdcfd95cde89b91f9e2dee1b3502c42cc117
dval <dylan.valentine@gmail.com>
2015-04-21 14:24:40 -0400

    updated config structure

--- php/pokio-config.php
+++ php/pokio-config.php
@@ -7,7 +7,7 @@
 $pokio_cfg['name']="Pokio the awesome!";
 
 //path from server root
-$pokio_cfg['domain']=['localhost','127.0.0.1'];
+$pokio_cfg['allowedHosts']=['localhost','127.0.0.1'];
 
 //path from server root
 $pokio_cfg['root']='/pokio';
@@ -15,18 +15,25 @@ $pokio_cfg['root']='/pokio';
 //allow http as well as https?
 $pokio_cfg['http']=true;
 
+//A unique pass phrase for this install
+$pokio_cfg['salt']="I am the Walrus. Ko0 Ko0 Katcho0";
+
 //changing this will attempt to create a new file.
 //relative to pokio install
 $pokio_cfg['statfile']="/stats/ratings.csv";
 
-//use db instead of csv file
-$pokio_cfg['db-type']=false;
+//how to store pok counts
+$pokio_cfg['logType']=POKIO_JSON;  //POKIO_DATABASE //POKIO_CSV
 
-//only work if 'db-on' is false.
+//how to return pok counts
+$pokio_cfg['return']=POKIO_JSON;  //POKIO_HTML //POKIO_CSV
+
+//only used if 'logtype' is POKIO_DATABASE.
 //relative to pokio install
-$pokio_cfg['db-path']="/path/to/sqlite.db";
 
-//only work if 'db-on' is true.
-$pokio_cfg['dbhost']="";
-$pokio_cfg['dblogin']="";
-$pokio_cfg['dbpass']="";
+#$pokio_cfg['db-name']="/path/to/sqlite.db";
+//OR:
+#$pokio_cfg['db-host']="mysql-host";
+#$pokio_cfg['db-login']="mysql-username";
+#$pokio_cfg['db-pass']="mysql-password";
+#$pokio_cfg['db-name']="mysql-database";
